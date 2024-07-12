20617afb634ac6f55acad29483b28a2a2e7f4ef7
amandaputri04/latihan2 <amandaputrii004@icloud.com>
2024-07-12 18:41:20 +0800

    first init github

---
+++ UAS_VISUAL2_AmandaPutriAzzahra/penjualan (1).sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 12, 2024 at 12:36 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `penjualan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kustomer`
+--
+
+CREATE TABLE `kustomer` (
+  `id` int(8) NOT NULL,
+  `nik` int(16) NOT NULL,
+  `nama` char(100) NOT NULL,
+  `telp` char(20) NOT NULL,
+  `email` char(100) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `member` char(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kustomer`
+--
+
+INSERT INTO `kustomer` (`id`, `nik`, `nama`, `telp`, `email`, `alamat`, `member`) VALUES
+(5, 2147483647, 'Amanda Putri Azzahra', '0823511224', 'amandaputriazzahra@gmial.com', 'Jl.Padat Karya', 'Yes');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kustomer`
+--
+ALTER TABLE `kustomer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kustomer`
+--
+ALTER TABLE `kustomer`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
